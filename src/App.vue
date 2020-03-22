1ef7c455315cc220dd6879437295083d95ff30cd
kevhunte <kevinmhunte@gmail.com>
2020-03-22 14:27:45 -0400

    Layout design + site icon -- create components

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,33 @@
 <div id="app">
   <!--<img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  ---- Navbar ----
-  <h4>
-    Scheduling Visualizer
-  </h4>
-  ----- Scheduling Components -----
+  <div id="navContainer">
+    <b-navbar type="dark" variant="dark" class="fixed-top">
+      <b-navbar-brand href="#">Scheduling Visualizer</b-navbar-brand>
+
+      <!--<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>-->
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item-dropdown text="Algorithm">
+            <b-dropdown-item href="#">FCFS</b-dropdown-item>
+            <b-dropdown-item href="#">LRJF</b-dropdown-item>
+            <b-dropdown-item href="#">Round Robin</b-dropdown-item>
+            <b-dropdown-item href="#">Priority Queue</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
+  </div>
+  <div id="otherData" class="">
+    <h4>
+      Scheduling Visualizer
+    </h4>
+    ------ Input for # of processes. Tell users to select alg from nav. Make active alg a different color ------- <br>
+    Display number of processes and Algorithm here <br>
+    ----- Scheduling Components in v-if to render with choice -----
+  </div>
 </div>
 </template>
 
@@ -17,6 +39,14 @@ export default {
   name: 'App',
   components: {
     //HelloWorld
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
   }
 }
 </script>
@@ -28,6 +58,13 @@ export default {
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+}
+
+#otherData {
+    padding-top: 80px;
+}
+
+.dropdown-toggle:after {
+    content: none;
 }
 </style>
